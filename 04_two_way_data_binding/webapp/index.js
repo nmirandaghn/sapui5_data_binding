d1017efb95eaa730030327eb11f45bf1ead66865
Nelson Miranda <nmiranda@slc.hn>
2023-12-22 08:32:06 -0600

    Two-way data binding

---
+++ 04_two_way_data_binding/webapp/index.js
@@ -0,0 +1,27 @@
+sap.ui.require([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView"
+], function (
+	JSONModel,
+	XMLView) {
+	"use strict";
+
+	// Attach an anonymous function to the SAPUI5 'init' event
+	sap.ui.getCore().attachInit(function () {
+		// Create a JSON model from an object literal
+		var oModel = new JSONModel({
+			firstName: "Harry",
+			lastName: "Hawk",
+			enabled: true,
+			panelHeaderText: "Data Binding basics"
+		});
+
+		// Assign the model object to the SAPUI5 core
+		sap.ui.getCore().setModel(oModel);
+
+		// Display the XML view called "App"
+		new XMLView({
+			viewName: "n.miranda.db.view.App"
+		}).placeAt("content");
+	});
+});
